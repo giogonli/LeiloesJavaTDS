1af585bec4f21f216eaa009081eae18d477b1123
unknown <contatogiovannalima12@gmail.com>
2024-10-24 20:19:33 -0300

    Remoção de comentários

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,4 +1,3 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -6,15 +5,6 @@ import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
